70b74e26ed759804734c420dd8404a5d0225ca77
nowaveosu <nowaveosu@gmail.com>
2024-09-24 23:17:37 +0900

    [Delete] statusbar import 삭제

--- RN-test/App.js
+++ RN-test/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>hello world!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
