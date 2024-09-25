e8b26d23ac5f527ff6643ab40e1d053707822b6e
nowaveosu <nowaveosu@gmail.com>
2024-09-25 21:54:15 +0900

    [Add] 버튼추가

--- RN-test/App.js
+++ RN-test/App.js
@@ -1,10 +1,16 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
+//html, css 없음. 유사한 요소들이 존재함
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
+      <View>
+        <Text>한글</Text>
+      </View>
+      <Text>hi</Text>
+      <Text>hello 3worsd!</Text>
+      <Button title='tap me!'/>
     </View>
   );
 }
