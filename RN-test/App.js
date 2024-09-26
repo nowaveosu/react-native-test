51b0285f4551ed58b54303d49d271f4c7c998cee
nowaveosu <nowaveosu@gmail.com>
2024-09-26 16:43:32 +0900

    [Style] flexbox 설정

--- RN-test/App.js
+++ RN-test/App.js
@@ -2,6 +2,7 @@
 import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 //html, css 없음. 유사한 요소들이 존재함, 코어컴포넌트를 직접import해야함
+// flexbox가 기본값임, 
 export default function App() {
   return (
     <View style={styles.appContainer}>
@@ -9,7 +10,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Your course goal!'/>
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...</Text>
       </View>
     </View>
@@ -18,17 +19,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
-    padding: 70,
+    flex: 1,
+    paddingTop: 70,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
   },
   textInput:{
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   }
 });
