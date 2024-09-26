538688421d1d9215c76c8014d049f8c08f4454c4
nowaveosu <nowaveosu@gmail.com>
2024-09-26 17:32:08 +0900

    [Add] map으로 state 뿌리기

--- RN-test/App.js
+++ RN-test/App.js
@@ -1,60 +1,71 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 
 //html, css 없음. 유사한 요소들이 존재함, 코어컴포넌트를 직접import해야함
-// flexbox가 기본값임, 
+// flexbox가 기본값임,
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState('');
-
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
-  };
+  }
 
   function addGoalHandler() {
-    console.log(enteredGoalText);
-  };
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  }
 
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
           style={styles.textInput}
-          placeholder='Your course goal!' 
+          placeholder="Your course goal!"
           onChangeText={goalInputHandler}
         />
         <Button onPress={addGoalHandler} title="Add Goal" />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
+  appContainer: {
     flex: 1,
     paddingTop: 70,
     paddingHorizontal: 16,
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    borderBottomColor: "#cccccc",
   },
-  textInput:{
+  textInput: {
     borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
+    borderColor: "#cccccc",
+    width: "70%",
     marginRight: 8,
     padding: 8,
   },
   goalsContainer: {
     flex: 5,
-  }
+  },
 });
