abc6276dbd1996521fdf07ee4c6d645483ad09f8
nowaveosu <nowaveosu@gmail.com>
2024-09-30 19:43:39 +0900

    [Add] 삭제 완성

--- RN-test/App.js
+++ RN-test/App.js
@@ -24,8 +24,10 @@ export default function App() {
     ]);
   }
 
-  function deleteGoalHandler() {
-    console.log("DELETE");
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
@@ -38,6 +40,7 @@ export default function App() {
             return (
               <GoalItem
                 text={itemData.item.text}
+                id={itemData.item.id}
                 onDeleteItem={deleteGoalHandler}
               />
             );
